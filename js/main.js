a963245487721d0b92906c941bdde43402821774
William Ahlberg <william.s.ahlberg@gmail.com>
2025-08-05 16:26:19 +0200

    Light mode

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function toggleDarkMode() {
     var element = document.body;
-    element.classList.toggle("dark-mode");
+    element.classList.toggle("light-mode");
 }
 
 document.getElementById("dark-mode-toggle").addEventListener("click", toggleDarkMode);
