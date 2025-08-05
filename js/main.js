7583e35f4011a919ab455d78356495dcff27ec33
William Ahlberg <william.s.ahlberg@gmail.com>
2025-08-05 16:15:01 +0200

    Added some content
    Added some content

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+document.getElementById("dark-mode-toggle").addEventListener("click", toggleDarkMode);
